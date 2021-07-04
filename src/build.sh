c58666977d887ccc4d6aa315e2ba8a06ac60de8c
Smitten <77771103+C84186@users.noreply.github.com>
2021-07-04 17:50:33 +1000

    update build script to use natural sort to iterate over src dir

--- src/build.sh
+++ src/build.sh
@@ -4,8 +4,16 @@ output_path="../e-hentai-downloader.user.js"
 meta_js_path="../e-hentai-downloader.meta.js"
 parts_js_dir="./"
 
-cat $meta_js_path>$output_path
-cat $parts_js_dir"first.js">>$output_path
-cat $parts_js_dir"JSZip.js">>$output_path
-cat $parts_js_dir"FileSaver.js">>$output_path
-cat $parts_js_dir"main.js">>$output_path
+# clear the bundle
+echo "" >"$output_path"
+
+find "$parts_js_dir" -maxdepth 1 -follow -type f -print | sort -V | while read -r f; do
+    case "$f" in
+        *.js)
+            cat "$f">>"$output_path"
+            ;;
+        *) echo "$0: Ignoring $f";;
+    esac
+done
+
+browserify "$output_path" -o "$output_path"
